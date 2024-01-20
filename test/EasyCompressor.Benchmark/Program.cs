b412b7cb0856bd1f7e5fe268c27233f81a7cd5c5
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-01-21 01:03:24 +0330

    BenchmarkDotNetVisualizer package added

--- test/EasyCompressor.Benchmark/Program.cs
+++ test/EasyCompressor.Benchmark/Program.cs
@@ -1,11 +1,3 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNetVisualizer;
 
-#if DEBUG
-System.Console.ForegroundColor = System.ConsoleColor.Yellow;
-System.Console.WriteLine("*****To achieve accurate results, set project configuration to Release mode.*****");
-return;
-#endif
-
-#pragma warning disable CS0162 // Unreachable code detected
-BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run();
-#pragma warning restore CS0162 // Unreachable code detected
+BenchmarkAutoRunner.SwitcherRun(typeof(Program).Assembly);
