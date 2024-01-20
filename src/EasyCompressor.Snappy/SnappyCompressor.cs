c050214cd2832ae8531126c23f95f3c16576a8f4
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-01-21 01:51:41 +0330

    SpappyCompressor code improved

--- src/EasyCompressor.Snappy/SnappyCompressor.cs
+++ src/EasyCompressor.Snappy/SnappyCompressor.cs
@@ -1,94 +1,94 @@
 ﻿using Snappy;
+using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace EasyCompressor
+namespace EasyCompressor;
+
+/// <summary>
+/// Snappy compressor
+/// </summary>
+public class SnappyCompressor : BaseCompressor
 {
+
+    /// <inheritdoc/>
+    public override CompressionMethod Method => CompressionMethod.Snappy;
+
     /// <summary>
-    /// Snappy compressor
+    /// Initializes a new instance
     /// </summary>
-    public class SnappyCompressor : BaseCompressor
+    /// <param name="name">Name</param>
+    public SnappyCompressor(string name = null)
     {
+        Name = name;
+    }
 
-        /// <inheritdoc/>
-        public override CompressionMethod Method => CompressionMethod.Snappy;
+    /// <inheritdoc/>
+    protected override byte[] BaseCompress(byte[] bytes)
+    {
+        return SnappyCodec.Compress(bytes);
+    }
 
-        /// <summary>
-        /// Initializes a new instance
-        /// </summary>
-        /// <param name="name">Name</param>
-        public SnappyCompressor(string name = null)
-        {
-            Name = name;
-        }
+    /// <inheritdoc/>
+    protected override byte[] BaseDecompress(byte[] compressedBytes)
+    {
+        return SnappyCodec.Uncompress(compressedBytes);
+    }
 
-        /// <inheritdoc/>
-        protected override byte[] BaseCompress(byte[] bytes)
-        {
-            return SnappyCodec.Compress(bytes);
-        }
+    /// <inheritdoc/>
+    protected override void BaseCompress(Stream inputStream, Stream outputStream)
+    {
+        var bytes = inputStream.ReadAllBytes();
+        var compressedBytes = BaseCompress(bytes);
 
-        /// <inheritdoc/>
-        protected override byte[] BaseDecompress(byte[] compressedBytes)
-        {
-            return SnappyCodec.Uncompress(compressedBytes);
-        }
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+        outputStream.WriteAllBytes((ReadOnlySpan<byte>)compressedBytes);
+#else
+        outputStream.WriteAllBytes(compressedBytes);
+#endif
+        outputStream.Flush();
+    }
 
-        /// <inheritdoc/>
-        protected override void BaseCompress(Stream inputStream, Stream outputStream)
-        {
-            using var inputMemory = new MemoryStream();
-            inputStream.CopyTo(inputMemory);
-            inputStream.Flush();
-            inputMemory.Flush();
+    /// <inheritdoc/>
+    protected override void BaseDecompress(Stream inputStream, Stream outputStream)
+    {
+        var compressedBytes = inputStream.ReadAllBytes();
+        var bytes = BaseDecompress(compressedBytes);
 
-            var compressedBytes = BaseCompress(inputMemory.ToArray());
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+        outputStream.WriteAllBytes((ReadOnlySpan<byte>)bytes);
+#else
+        outputStream.WriteAllBytes(bytes);
+#endif
+        outputStream.Flush();
+    }
 
-            outputStream.Write(compressedBytes, 0, compressedBytes.Length);
-            outputStream.Flush();
-        }
+    /// <inheritdoc/>
+    protected override async Task BaseCompressAsync(Stream inputStream, Stream outputStream, CancellationToken cancellationToken = default)
+    {
+        var bytes = await inputStream.ReadAllBytesAsync(cancellationToken).ConfigureAwait(false);
+        var compressedBytes = BaseCompress(bytes);
 
-        /// <inheritdoc/>
-        protected override void BaseDecompress(Stream inputStream, Stream outputStream)
-        {
-            using var inputMemory = new MemoryStream();
-            inputStream.CopyTo(inputMemory, DefaultBufferSize);
-            inputStream.Flush();
-            inputMemory.Flush();
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+        await outputStream.WriteAllBytesAsync((ReadOnlyMemory<byte>)compressedBytes, cancellationToken).ConfigureAwait(false);
+#else
+        await outputStream.WriteAllBytesAsync(compressedBytes, cancellationToken).ConfigureAwait(false);
+#endif
+        await outputStream.FlushAsync(cancellationToken).ConfigureAwait(false);
+    }
 
-            var compressedBytes = BaseDecompress(inputMemory.ToArray());
+    /// <inheritdoc/>
+    protected override async Task BaseDecompressAsync(Stream inputStream, Stream outputStream, CancellationToken cancellationToken = default)
+    {
+        var compressedBytes = await inputStream.ReadAllBytesAsync(cancellationToken).ConfigureAwait(false);
+        var bytes = BaseDecompress(compressedBytes);
 
-            outputStream.Write(compressedBytes, 0, compressedBytes.Length);
-            outputStream.Flush();
-        }
-
-        /// <inheritdoc/>
-        protected override async Task BaseCompressAsync(Stream inputStream, Stream outputStream, CancellationToken cancellationToken = default)
-        {
-            using var inputMemory = new MemoryStream();
-            await inputStream.CopyToAsync(inputMemory, DefaultBufferSize, cancellationToken).ConfigureAwait(false);
-            await inputStream.FlushAsync(cancellationToken).ConfigureAwait(false);
-            await inputMemory.FlushAsync(cancellationToken).ConfigureAwait(false);
-
-            var compressedBytes = BaseCompress(inputMemory.ToArray());
-
-            await outputStream.WriteAsync(compressedBytes, 0, compressedBytes.Length, cancellationToken).ConfigureAwait(false);
-            await outputStream.FlushAsync().ConfigureAwait(false);
-        }
-
-        /// <inheritdoc/>
-        protected override async Task BaseDecompressAsync(Stream inputStream, Stream outputStream, CancellationToken cancellationToken = default)
-        {
-            using var inputMemory = new MemoryStream();
-            await inputStream.CopyToAsync(inputMemory, DefaultBufferSize, cancellationToken).ConfigureAwait(false);
-            await inputStream.FlushAsync(cancellationToken).ConfigureAwait(false);
-            await inputMemory.FlushAsync(cancellationToken).ConfigureAwait(false);
-
-            var compressedBytes = BaseDecompress(inputMemory.ToArray());
-
-            await outputStream.WriteAsync(compressedBytes, 0, compressedBytes.Length, cancellationToken).ConfigureAwait(false);
-            await outputStream.FlushAsync().ConfigureAwait(false);
-        }
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+        await outputStream.WriteAllBytesAsync((ReadOnlyMemory<byte>)bytes, cancellationToken).ConfigureAwait(false);
+#else
+        await outputStream.WriteAllBytesAsync(bytes, cancellationToken).ConfigureAwait(false);
+#endif
+        await outputStream.FlushAsync(cancellationToken).ConfigureAwait(false);
     }
 }
