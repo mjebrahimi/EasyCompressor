938867ebb3c647aea6a8f02096252e9c3f84c2f2
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-01-22 01:46:16 +0330

    .

--- test/EasyCompressor.Tests/StreamTests.cs
+++ test/EasyCompressor.Tests/StreamTests.cs
@@ -7,139 +7,193 @@ namespace EasyCompressor.Tests.StreamTests;
 
 public class StreamTests(ICompressor compressor) : TestBase(compressor)
 {
-    public class StreamTests : TestBase
-    {
-        public StreamTests(ICompressor compressor)
-            : base(compressor)
-        {
-        }
+    //[Test]
+    //public void Compress_Large_Data()
+    //{
+    //    var size2GB = 2_147_483_648;
+    //    var filePath = "D:\\large_file.dump";
 
-        [Test]
-        public void Compress_NullValue1_Should_Throws_ArgumentNullException()
-        {
+    //    try
+    //    {
+    //        CreateDumpFile(filePath, size2GB);
+
+    //        var file = File.OpenRead(filePath);
+    //        var output = File.OpenWrite(filePath + ".7z");
+
+    //        var compressor = new LZMACompressor();
+    //        compressor.Compress(file, output);
+    //    }
+    //    finally
+    //    {
+    //        File.Delete(filePath);
+    //    }
+    //}
+
+    //[Test]
+    //public void Decompress_Large_Data()
+    //{
+    //    var path = "D:\\large_data";
+    //    var compressor = new LZMACompressor();
+    //    var file = File.OpenRead(path + "_compressed");
+    //    var output = File.OpenWrite(path + "_decompressed");
+    //    compressor.Decompress(file, output);
+    //}
+
+    [Test]
+    public void Compress_NullValue1_Should_Throws_ArgumentNullException()
+    {
         void action() => Compressor.Compress(null, new MemoryStream());
 
-            Assert.Throws<ArgumentNullException>(action);
-        }
+        Assert.Throws<ArgumentNullException>(action);
+    }
 
-        [Test]
-        public void Compress_NullValue2_Should_Throws_ArgumentNullException()
-        {
+    [Test]
+    public void Compress_NullValue2_Should_Throws_ArgumentNullException()
+    {
         void action() => Compressor.Compress(new MemoryStream(), null);
 
-            Assert.Throws<ArgumentNullException>(action);
-        }
+        Assert.Throws<ArgumentNullException>(action);
+    }
 
-        [Test]
-        public void Decompress_NullValue1_Should_Throws_ArgumentNullException()
-        {
+    [Test]
+    public void Decompress_NullValue1_Should_Throws_ArgumentNullException()
+    {
         void action() => Compressor.Decompress(null, new MemoryStream());
 
-            Assert.Throws<ArgumentNullException>(action);
-        }
+        Assert.Throws<ArgumentNullException>(action);
+    }
 
-        [Test]
-        public void Decompress_NullValue2_Should_Throws_ArgumentNullException()
-        {
+    [Test]
+    public void Decompress_NullValue2_Should_Throws_ArgumentNullException()
+    {
         void action() => Compressor.Decompress(new MemoryStream(), null);
 
-            Assert.Throws<ArgumentNullException>(action);
-        }
+        Assert.Throws<ArgumentNullException>(action);
+    }
 
-        [Test]
-        public void Compress_Should_DoesNotThrow_Exception()
-        {
-            using var inputStream = new MemoryStream(ObjectBytes);
-            using var outputStream = new MemoryStream();
+    [Test]
+    public void Compress_Should_DoesNotThrow_Exception()
+    {
+        using var inputStream = new MemoryStream(ObjectBytes);
+        using var outputStream = new MemoryStream();
 
         void action() => Compressor.Compress(inputStream, outputStream);
 
-            Assert.DoesNotThrow(action);
-        }
+        Assert.DoesNotThrow(action);
+    }
 
-        [Test]
-        public void CompressedResult_ShouldNot_NullOrEmpty()
-        {
-            using var inputStream = new MemoryStream(ObjectBytes);
-            using var compressedStream = new MemoryStream();
+    [Test]
+    public void CompressedResult_ShouldNot_NullOrEmpty()
+    {
+        using var inputStream = new MemoryStream(ObjectBytes);
+        using var compressedStream = new MemoryStream();
 
-            Compressor.Compress(inputStream, compressedStream);
+        Compressor.Compress(inputStream, compressedStream);
 
-            var compressedBytes = compressedStream.ToArray();
+        var compressedBytes = compressedStream.ToArray();
 
         Assert.That(compressedBytes, Is.Not.Null);
         Assert.That(compressedBytes, Is.Not.Empty);
-        }
+    }
 
-        [Test]
-        public void Decompress_Should_DoesNotThrow_Exception()
-        {
-            using var inputStream = new MemoryStream(ObjectBytes);
-            using var outputStream = new MemoryStream();
+    [Test]
+    public void Decompress_Should_DoesNotThrow_Exception()
+    {
+        using var inputStream = new MemoryStream(ObjectBytes);
+        using var outputStream = new MemoryStream();
 
-            Compressor.Compress(inputStream, outputStream);
-            var compressedBytes = outputStream.ToArray();
+        Compressor.Compress(inputStream, outputStream);
+        var compressedBytes = outputStream.ToArray();
 
-            using var inputStream2 = new MemoryStream(compressedBytes);
-            using var outputStream2 = new MemoryStream();
+        using var inputStream2 = new MemoryStream(compressedBytes);
+        using var outputStream2 = new MemoryStream();
 
         void action() => Compressor.Decompress(inputStream2, outputStream2);
 
-            Assert.DoesNotThrow(action);
-        }
+        Assert.DoesNotThrow(action);
+    }
 
-        [Test]
+    [Test]
     public void DecompressedResult_ShouldNot_NullOrEmpty()
-        {
-            using var inputStream = new MemoryStream(ObjectBytes);
-            using var outputStream = new MemoryStream();
+    {
+        using var inputStream = new MemoryStream(ObjectBytes);
+        using var outputStream = new MemoryStream();
 
-            Compressor.Compress(inputStream, outputStream);
-            var compressedBytes = outputStream.ToArray();
+        Compressor.Compress(inputStream, outputStream);
+        var compressedBytes = outputStream.ToArray();
 
-            using var inputStream2 = new MemoryStream(compressedBytes);
-            using var outputStream2 = new MemoryStream();
+        using var inputStream2 = new MemoryStream(compressedBytes);
+        using var outputStream2 = new MemoryStream();
 
-            Compressor.Decompress(inputStream2, outputStream2);
-            var decompressedBytes = outputStream2.ToArray();
+        Compressor.Decompress(inputStream2, outputStream2);
+        var decompressedBytes = outputStream2.ToArray();
 
-            Assert.IsNotNull(decompressedBytes);
-            Assert.IsNotEmpty(decompressedBytes);
-        }
+        Assert.That(decompressedBytes, Is.Not.Null);
+        Assert.That(decompressedBytes, Is.Not.Empty);
+    }
 
-        [Test]
-        public void DecompressedResult_Should_SequenceEqual_With_SourceBytes()
-        {
-            using var inputStream = new MemoryStream(ObjectBytes);
-            using var outputStream = new MemoryStream();
+    [Test]
+    public void DecompressedResult_Should_SequenceEqual_With_SourceBytes()
+    {
+        using var inputStream = new MemoryStream(ObjectBytes);
+        using var outputStream = new MemoryStream();
 
-            Compressor.Compress(inputStream, outputStream);
-            var compressedBytes = outputStream.ToArray();
+        Compressor.Compress(inputStream, outputStream);
+        var compressedBytes = outputStream.ToArray();
 
-            using var inputStream2 = new MemoryStream(compressedBytes);
-            using var outputStream2 = new MemoryStream();
+        using var inputStream2 = new MemoryStream(compressedBytes);
+        using var outputStream2 = new MemoryStream();
 
-            Compressor.Decompress(inputStream2, outputStream2);
-            var decompressedBytes = outputStream2.ToArray();
+        Compressor.Decompress(inputStream2, outputStream2);
+        var decompressedBytes = outputStream2.ToArray();
 
         Assert.That(decompressedBytes.SequenceEqual(ObjectBytes), Is.True);
-        }
-
-        [Test]
-        public void CompressAndDecompress_ShouldNot_Close_BothStreams()
-        {
-            using var inputStream = new MemoryStream(ObjectBytes);
-            using var outputStream = new MemoryStream();
-            Compressor.Compress(inputStream, outputStream);
-
-            Assert.DoesNotThrow(() => inputStream.Position = 0);
-            Assert.DoesNotThrow(() => outputStream.Position = 0);
-
-            using var outputStream2 = new MemoryStream();
-            Compressor.Decompress(outputStream, outputStream2);
-
-            Assert.DoesNotThrow(() => outputStream.Position = 0);
-            Assert.DoesNotThrow(() => outputStream2.Position = 0);
-        }
     }
+
+    [Test]
+    public void CompressAndDecompress_ShouldNot_Close_BothStreams()
+    {
+        using var inputStream = new MemoryStream(ObjectBytes);
+        using var outputStream = new MemoryStream();
+        Compressor.Compress(inputStream, outputStream);
+
+        Assert.DoesNotThrow(() => inputStream.Position = 0);
+        Assert.DoesNotThrow(() => outputStream.Position = 0);
+
+        using var outputStream2 = new MemoryStream();
+        Compressor.Decompress(outputStream, outputStream2);
+
+        Assert.DoesNotThrow(() => outputStream.Position = 0);
+        Assert.DoesNotThrow(() => outputStream2.Position = 0);
+    }
+
+    #region CreateDumpFile
+    private static void CreateDumpFile(string filePath, long fileSize)
+    {
+        using var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+        CreateDumpFile(fileStream, fileSize);
+    }
+
+    private static void CreateDumpFile(FileStream fileStream, long fileSize)
+    {
+        const int maxArraySize = 2_147_483_591;
+        if (fileSize <= maxArraySize)
+        {
+            byte[] buffer = new byte[fileSize];
+            fileStream.Write(buffer);
+        }
+        else
+        {
+            //TODO
+            long currentFileSize = 0;
+            while (currentFileSize < fileSize)
+            {
+                byte[] buffer = new byte[1024];
+                fileStream.Write(buffer);
+                currentFileSize += buffer.Length;
+            }
+        }
+        fileStream.FlushAsync();
+    }
+    #endregion
 }
