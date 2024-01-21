91f156e29624cae348922222b5a468704a0fc218
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-01-21 22:28:42 +0330

    StreamExtensions code improved and bug fixed

--- src/EasyCompressor/Extensions/StreamExtensions.cs
+++ src/EasyCompressor/Extensions/StreamExtensions.cs
@@ -21,33 +21,42 @@ public static class StreamExtensions
     /// <returns></returns>
     public static byte[] GetTrimmedBuffer(this MemoryStream stream)
     {
+        var length = (int)stream.Length;
         var bytes = stream.GetBuffer();
-        Array.Resize(ref bytes, (int)stream.Length);
+        if (length < bytes.Length)
+            Array.Resize(ref bytes, length);
         return bytes;
     }
 
     #region ReadAllBytes
     /// <summary>
-    /// Reads all bytes.
+    /// Reads all [Remained] bytes from current <see cref="Stream.Position"/> to End.
     /// </summary>
     /// <param name="stream">The stream.</param>
     /// <returns></returns>
     public static byte[] ReadAllBytes(this Stream stream)
     {
-        if (stream is MemoryStream memory)
-            return memory.ToArray();
+        if (stream.CanSeek is false)
+        {
+            using var output = new MemoryStream();
+            stream.CopyTo(output);
+            return output.GetTrimmedBuffer();
+        }
 
 #if NET5_0_OR_GREATER
-        var bytes = GC.AllocateUninitializedArray<byte>((int)stream.Length);
+        var bytes = GC.AllocateUninitializedArray<byte>((int)(stream.Length - stream.Position));
 #else
-        var bytes = new byte[(int)stream.Length];
+        var bytes = new byte[(int)(stream.Length - stream.Position)];
 #endif
 
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+        /*var bytesRead = */
         stream.Read(bytes.AsSpan());
 #else
-        stream.Read(bytes, 0, bytes.Length);
+        /*var bytesRead = */stream.Read(bytes, 0, bytes.Length);
 #endif
+        //if (bytesRead != bytes.Length)
+        //    Array.Resize(ref bytes, bytesRead);
         return bytes;
     }
 
@@ -59,20 +68,31 @@ public static class StreamExtensions
     /// <returns></returns>
     public static async Task<byte[]> ReadAllBytesAsync(this Stream stream, CancellationToken cancellationToken = default)
     {
-        if (stream is MemoryStream memory)
-            return memory.ToArray();
+        if (stream.CanSeek is false)
+        {
+            using var output = new MemoryStream();
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+            await stream.CopyToAsync(output, cancellationToken).ConfigureAwait(false);
+#else
+            await stream.CopyToAsync(output, 81920, cancellationToken).ConfigureAwait(false); // DefaultBufferSize: 81920
+#endif
+            return output.GetTrimmedBuffer();
+        }
 
 #if NET5_0_OR_GREATER
-        var bytes = GC.AllocateUninitializedArray<byte>((int)stream.Length);
+        var bytes = GC.AllocateUninitializedArray<byte>((int)(stream.Length - stream.Position));
 #else
-        var bytes = new byte[(int)stream.Length];
+        var bytes = new byte[(int)(stream.Length - stream.Position)];
 #endif
 
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER || NET5_0_OR_GREATER
+        /*var bytesRead = */
         await stream.ReadAsync(bytes.AsMemory(), cancellationToken);
 #else
-        await stream.ReadAsync(bytes, 0, bytes.Length, cancellationToken);
+        /*var bytesRead = */await stream.ReadAsync(bytes, 0, bytes.Length, cancellationToken);
 #endif
+        //if (bytesRead != bytes.Length)
+        //    Array.Resize(ref bytes, bytesRead);
         return bytes;
     }
     #endregion
@@ -139,3 +159,48 @@ public static class StreamExtensions
     }
     #endregion
 }
+
+
+#region Functionality Tests
+////====================================== Bellow than internal buffer size ======================================
+//{
+//    var arr = Enumerable.Range(1, 6).Select(p => (byte)p).ToArray();
+//    using var stream = new MemoryStream();
+//    stream.WriteAllBytes(arr);
+//    stream.Position = 2;
+//    var bytes = stream.ReadAllBytes_UsingBinaryReader();
+//}
+//{
+//    var arr = Enumerable.Range(1, 6).Select(p => (byte)p).ToArray();
+//    using var stream = new MemoryStream();
+//    stream.WriteAllBytes(arr);
+//    stream.Position = 2;
+//    var bytes = stream.ReadAllBytes_UsingCopyToMemoryStream();
+//}
+//{
+//    var arr = Enumerable.Range(1, 6).Select(p => (byte)p).ToArray();
+//    using var stream = new MemoryStream();
+//    stream.WriteAllBytes(arr);
+//    stream.Position = 2;
+//    var bytes = stream.ReadAllBytes();
+//}
+////=================================== With specified range of array =========================================
+//{
+//    var arr = Enumerable.Range(1, 100).Select(p => (byte)p).ToArray();
+//    using var stream = new MemoryStream(arr, 20, 30);
+//    stream.Position = 15;
+//    var bytes = stream.ReadAllBytes_UsingBinaryReader();
+//}
+//{
+//    var arr = Enumerable.Range(1, 100).Select(p => (byte)p).ToArray();
+//    using var stream = new MemoryStream(arr, 20, 30);
+//    stream.Position = 15;
+//    var bytes = stream.ReadAllBytes_UsingCopyToMemoryStream();
+//}
+//{
+//    var arr = Enumerable.Range(1, 100).Select(p => (byte)p).ToArray();
+//    using var stream = new MemoryStream(arr, 20, 30);
+//    stream.Position = 15;
+//    var bytes = stream.ReadAllBytes();
+//}
+#endregion
