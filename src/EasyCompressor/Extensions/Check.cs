d398eca168ba1ed72a5d9f1310cf6cb82f68179f
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-01-23 21:54:04 +0330

    .

--- src/EasyCompressor/Extensions/Check.cs
+++ src/EasyCompressor/Extensions/Check.cs
@@ -8,70 +8,27 @@ namespace EasyCompressor.Internal;
 public static class Check
 {
     /// <summary>
-    /// Validates that obj is not null , otherwise throws ArgumentNullException exception.
+    /// Validates that obj is not null , otherwise throws an ArgumentNullException exception.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    /// <param name="obj"></param>
-    /// <param name="name">Name</param>
-    /// <param name="message"></param>
+    /// <param name="obj">The object.</param>
+    /// <param name="name">The name.</param>
+    /// <param name="message">The message.</param>
     /// <returns></returns>
     public static T NotNull<T>(this T obj, string name, string message = null)
     {
-        if (obj == null)
-            throw new ArgumentNullException($"{name} ({typeof(T)}", message);
+        if (obj is null)
+            throw new ArgumentNullException($"Argument '{name}' ({typeof(T)} most not null.", message);
         return obj;
     }
 
     /// <summary>
-    /// Validates that str is not null or white space , otherwise throws ArgumentNullException exception.
-    /// </summary>
-    /// <param name="str"></param>
-    /// <param name="name">Name</param>
-    /// <param name="message"></param>
-    /// <returns></returns>
-    public static string NotNullOrWhiteSpace(this string str, string name, string message = null)
-    {
-        if (string.IsNullOrWhiteSpace(str))
-            throw new ArgumentNullException(name, message);
-        return str;
-    }
-
-    /// <summary>
-    /// Validates that list is not null or empty , otherwise throws ArgumentNullException exception.
-    /// </summary>
-    /// <param name="str"></param>
-    /// <param name="name">Name</param>
-    /// <param name="message"></param>
-    /// <returns></returns>
-    public static string NotNullOrEmpty(this string str, string name, string message = null)
-    {
-        if (string.IsNullOrEmpty(str))
-            throw new ArgumentNullException(name, message);
-        return str;
-    }
-
-    ///// <summary>
-    ///// Checks that the list is not empty (throw ArgumentException if list is empty)
-    ///// </summary>
-    ///// <typeparam name="T"></typeparam>
-    ///// <param name="obj"></param>
-    ///// <param name="name">Name</param>
-    ///// <param name="message"></param>
-    ///// <returns></returns>
-    //public static T NotEmpty<T>(this T obj, string name, string message = null) where T : IEnumerable
-    //{
-    //    if (!obj.Cast<object>().Any())
-    //        throw new ArgumentException($"Argument {name} ({typeof(T)}) is empty. " + message, name);
-    //    return obj;
-    //}
-
-    /// <summary>
-    /// Checks that the list is not empty (throw ArgumentException if list is empty)
+    /// Validates that the array is not empty, otherwise throws an ArgumentException exception.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    /// <param name="obj"></param>
-    /// <param name="name">Name</param>
-    /// <param name="message"></param>
+    /// <param name="obj">The object.</param>
+    /// <param name="name">The name.</param>
+    /// <param name="message">The message.</param>
     /// <returns></returns>
     public static T[] NotEmpty<T>(this T[] obj, string name, string message = null)
     {
@@ -81,20 +38,17 @@ public static class Check
     }
 
     /// <summary>
-    /// Validates that list is not null or empty , otherwise throws an exception.
+    /// Validates that array is not null or empty , otherwise throws an exception.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    /// <param name="obj"></param>
-    /// <param name="name">Name</param>
-    /// <param name="message"></param>
+    /// <param name="obj">The object.</param>
+    /// <param name="name">The name.</param>
+    /// <param name="message">The message.</param>
     /// <returns></returns>
     public static T[] NotNullOrEmpty<T>(this T[] obj, string name, string message = null)
     {
-        if (obj == null)
-            throw new ArgumentNullException($"{name} ({typeof(T)}", message);
-
-        if (obj.Length == 0)
-            throw new ArgumentException($"Argument {name} ({typeof(T)}) is empty. " + message, name);
-        return obj;
+        return obj
+            .NotNull(name, message)
+            .NotEmpty(name, message);
     }
 }
