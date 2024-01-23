dafd7367181aa5ac2f7f81e80936cde4855c1dc8
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-01-23 21:54:56 +0330

    BrotliNET code improved - Obsolete added

--- src/EasyCompressor.BrotliNET/BrotliNETCompressor.cs
+++ src/EasyCompressor.BrotliNET/BrotliNETCompressor.cs
@@ -1,117 +1,132 @@
-﻿using System.IO;
+﻿// Ignore Spelling: Brotli
+
+using System;
+using System.IO;
 using System.IO.Compression;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace EasyCompressor
+namespace EasyCompressor;
+
+/// <summary>
+/// BrotliNET compressor
+/// </summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+#pragma warning disable S1133 // Deprecated code should be removed
+[Obsolete("Instead, use the built-in BrotliCompressor class in the EasyCompressor package. (for projects with target-framework NETStandard2.1 or NETCore2.1 or above)")]
+#pragma warning restore S1133 // Deprecated code should be removed
+#endif
+#pragma warning disable S101 // Types should be named in PascalCase
+public class BrotliNETCompressor : BaseCompressor
+#pragma warning restore S101 // Types should be named in PascalCase
 {
     /// <summary>
-    /// BrotliNET compressor
+    /// Quality
     /// </summary>
-    public class BrotliNETCompressor : BaseCompressor
+    protected readonly uint Quality;
+
+    /// <summary>
+    /// Window
+    /// </summary>
+    protected readonly uint Window;
+
+    /// <inheritdoc/>
+    public override CompressionMethod Method => CompressionMethod.Brotli;
+
+    /// <summary>
+    /// Initializes a new instance
+    /// </summary>
+    /// <param name="name">Name</param>
+    /// <param name="quality">Quality (Defaults to <c>5</c>)</param>
+    /// <param name="window">Window (Defaults to <c>22</c>)</param>
+    public BrotliNETCompressor(string name = null, uint quality = 5, uint window = 22)
     {
-        /// <summary>
-        /// Quality
-        /// </summary>
-        protected readonly uint Quality;
+        Name = name;
+        Quality = quality;
+        Window = window;
+    }
 
-        /// <summary>
-        /// Window
-        /// </summary>
-        protected readonly uint Window;
-
-        /// <inheritdoc/>
-        public override CompressionMethod Method => CompressionMethod.Brotli;
-
-        /// <summary>
-        /// Initializes a new instance
-        /// </summary>
-        /// <param name="name">Name</param>
-        /// <param name="quality">Quality</param>
-        /// <param name="window">Window</param>
-        public BrotliNETCompressor(string name = null, uint quality = 11, uint window = 22)
+    /// <inheritdoc/>
+    protected override byte[] BaseCompress(byte[] bytes)
+    {
+        using var outputStream = new MemoryStream();
+        using (var brotliStream = new Brotli.BrotliStream(outputStream, CompressionMode.Compress, leaveOpen: true))
         {
-            Name = name;
-            Quality = quality;
-            Window = window;
-        }
-
-        /// <inheritdoc/>
-        protected override byte[] BaseCompress(byte[] bytes)
-        {
-            using var inputStream = new MemoryStream(bytes);
-            using var outputStream = new MemoryStream();
-            using (var brotliStream = new Brotli.BrotliStream(outputStream, CompressionMode.Compress))
-            {
-                brotliStream.SetQuality(Quality);
-                brotliStream.SetWindow(Window);
-                inputStream.CopyTo(brotliStream, bytes.Length);
-                //inputStream.WriteTo(brotliStream);
-                //brotliStream.Write(bytes, 0, bytes.Length);
-
-                inputStream.Flush();
-                brotliStream.Flush();
-            }
-            return outputStream.ToArray();
-        }
-
-        /// <inheritdoc/>
-        protected override byte[] BaseDecompress(byte[] compressedBytes)
-        {
-            using var inputStream = new MemoryStream(compressedBytes);
-            using var outputStream = new MemoryStream();
-            using (var brotliStream = new Brotli.BrotliStream(inputStream, CompressionMode.Decompress))
-            {
-                brotliStream.CopyTo(outputStream, compressedBytes.Length);
-
-                outputStream.Flush();
-                brotliStream.Flush();
-            }
-            return outputStream.ToArray();
-        }
-
-        /// <inheritdoc/>
-        protected override void BaseCompress(Stream inputStream, Stream outputStream)
-        {
-            using var brotliStream = new Brotli.BrotliStream(outputStream, CompressionMode.Compress, true);
             brotliStream.SetQuality(Quality);
             brotliStream.SetWindow(Window);
-            inputStream.CopyTo(brotliStream);
+            brotliStream.WriteAllBytes(bytes);
 
-            inputStream.Flush();
-            brotliStream.Flush();
+            // Disposing the compressor also flushes the buffers to the inner stream
+            // We pass true to the constructor above so that it doesn't close/dispose the inner stream
+            // Alternatively, we could call brotliStream.Flush() //It's not necessary based on my experiments
         }
+        return outputStream.GetTrimmedBuffer();
+    }
 
-        /// <inheritdoc/>
-        protected override void BaseDecompress(Stream inputStream, Stream outputStream)
+    /// <inheritdoc/>
+    protected override byte[] BaseDecompress(byte[] compressedBytes)
+    {
+        using var inputStream = new MemoryStream(compressedBytes);
+        using var brotliStream = new Brotli.BrotliStream(inputStream, CompressionMode.Decompress, leaveOpen: true);
+        //brotliStream.Flush(); //It's not necessary based on my experiments
+        return brotliStream.ReadAllBytes();
+    }
+
+    /// <inheritdoc/>
+    protected override void BaseCompress(Stream inputStream, Stream outputStream)
+    {
+        using (var brotliStream = new Brotli.BrotliStream(outputStream, CompressionMode.Compress, leaveOpen: true))
         {
-            using var brotliStream = new Brotli.BrotliStream(inputStream, CompressionMode.Decompress, true);
-            brotliStream.CopyTo(outputStream);
-
-            outputStream.Flush();
-            brotliStream.Flush();
-        }
-
-        /// <inheritdoc/>
-        protected override async Task BaseCompressAsync(Stream inputStream, Stream outputStream, CancellationToken cancellationToken = default)
-        {
-            using var brotliStream = new Brotli.BrotliStream(outputStream, CompressionMode.Compress, true);
             brotliStream.SetQuality(Quality);
             brotliStream.SetWindow(Window);
-            await inputStream.CopyToAsync(brotliStream, DefaultBufferSize, cancellationToken).ConfigureAwait(false);
+            inputStream.CopyTo(brotliStream); //Don't pass buffer size
 
-            await inputStream.FlushAsync(cancellationToken).ConfigureAwait(false);
-            await brotliStream.FlushAsync(cancellationToken).ConfigureAwait(false);
+            //brotliStream.Flush(); //It's not necessary based on my experiments
         }
+        outputStream.Flush(); //It's needed because of FileStream internal buffering
+    }
 
-        /// <inheritdoc/>
-        protected override async Task BaseDecompressAsync(Stream inputStream, Stream outputStream, CancellationToken cancellationToken = default)
+    /// <inheritdoc/>
+    protected override void BaseDecompress(Stream inputStream, Stream outputStream)
+    {
+        using (var brotliStream = new Brotli.BrotliStream(inputStream, CompressionMode.Decompress, leaveOpen: true))
         {
-            using var brotliStream = new Brotli.BrotliStream(inputStream, CompressionMode.Decompress, true);
-            await brotliStream.CopyToAsync(outputStream, DefaultBufferSize, cancellationToken).ConfigureAwait(false);
+            brotliStream.CopyTo(outputStream); //Don't pass buffer size
 
-            await outputStream.FlushAsync(cancellationToken).ConfigureAwait(false);
-            await brotliStream.FlushAsync(cancellationToken).ConfigureAwait(false);
+            //brotliStream.Flush(); //It's not necessary based on my experiments
         }
+        outputStream.Flush(); //It's needed because of FileStream internal buffering
+    }
+
+    /// <inheritdoc/>
+    protected override async Task BaseCompressAsync(Stream inputStream, Stream outputStream, CancellationToken cancellationToken = default)
+    {
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+        await
+#endif
+        using (var brotliStream = new Brotli.BrotliStream(outputStream, CompressionMode.Compress, leaveOpen: true))
+        {
+            brotliStream.SetQuality(Quality);
+            brotliStream.SetWindow(Window);
+            await inputStream.CopyToAsync(brotliStream, cancellationToken).ConfigureAwait(false); //Don't pass buffer size
+
+            //await brotliStream.FlushAsync(cancellationToken).ConfigureAwait(false); //It adds some extra bytes but it's not necessary based on my experiments
+        }
+        await outputStream.FlushAsync(cancellationToken).ConfigureAwait(false); //It's needed because of FileStream internal buffering
+    }
+
+    /// <inheritdoc/>
+    protected override async Task BaseDecompressAsync(Stream inputStream, Stream outputStream, CancellationToken cancellationToken = default)
+    {
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+        await
+#endif
+        using (var brotliStream = new Brotli.BrotliStream(inputStream, CompressionMode.Decompress, leaveOpen: true))
+        {
+            await brotliStream.CopyToAsync(outputStream, cancellationToken).ConfigureAwait(false); //Don't pass buffer size
+
+            //await brotliStream.FlushAsync(cancellationToken).ConfigureAwait(false); //Flush only works when compressing (not when decompressing)
+        }
+        await outputStream.FlushAsync(cancellationToken).ConfigureAwait(false); //It's needed because of FileStream internal buffering
     }
 }
--- src/EasyCompressor.BrotliNET/ConfigurationExtensions.cs
+++ src/EasyCompressor.BrotliNET/ConfigurationExtensions.cs
@@ -1,41 +1,50 @@
-﻿using EasyCompressor;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+﻿// Ignore Spelling: Brotli
 
-namespace Microsoft.Extensions.DependencyInjection
+using EasyCompressor;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Configuration extensions
+/// </summary>
+public static class ConfigurationExtensions
 {
     /// <summary>
-    /// Configuration extensions
+    /// Add BrotliNETCompressor to services
     /// </summary>
-    public static class ConfigurationExtensions
+    /// <param name="services">services</param>
+    /// <param name="quality">Quality (Defaults to <c>5</c>)</param>
+    /// <param name="window">Window (Defaults to <c>22</c>)</param>
+    /// <returns>IServiceCollection</returns>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+#pragma warning disable S1133 // Deprecated code should be removed
+    [Obsolete("Instead, use the built-in BrotliCompressor class (using AddBrotliCompressor) in the EasyCompressor package. (for projects with target-framework NETStandard2.1 or NETCore2.1 or above)")]
+#pragma warning restore S1133 // Deprecated code should be removed
+#endif
+    public static IServiceCollection AddBrotliNETCompressor(this IServiceCollection services, uint quality = 5, uint window = 22)
     {
-        /// <summary>
-        /// Add BrotliNETCompressor to services
-        /// </summary>
-        /// <param name="services">services</param>
-        /// <param name="quality">Quality</param>
-        /// <param name="window">Window</param>
-        /// <returns>IServiceCollection</returns>
-        public static IServiceCollection AddBrotliNETCompressor(this IServiceCollection services, uint quality = 11, uint window = 22)
-        {
-            return services.AddBrotliNETCompressor(null, quality, window);
-        }
+        return services.AddBrotliNETCompressor(null, quality, window);
+    }
 
-        /// <summary>
-        /// Add BrotliNETCompressor to services with specified name
-        /// </summary>
-        /// <param name="services">services</param>
-        /// <param name="name">Name</param>
-        /// <param name="quality">Quality</param>
-        /// <param name="window">Window</param>
-        /// <returns>IServiceCollection</returns>
-        public static IServiceCollection AddBrotliNETCompressor(this IServiceCollection services, string name, uint quality = 11, uint window = 22)
-        {
-            services.TryAddSingleton<ICompressorProvider, DefaultCompressorProvider>();
-            services.AddSingleton<ICompressor, BrotliNETCompressor>(x =>
-            {
-                return new BrotliNETCompressor(name, quality, window);
-            });
-            return services;
-        }
+    /// <summary>
+    /// Add BrotliNETCompressor to services with specified name
+    /// </summary>
+    /// <param name="services">services</param>
+    /// <param name="name">Name</param>
+    /// <param name="quality">Quality (Defaults to <c>5</c>)</param>
+    /// <param name="window">Window (Defaults to <c>22</c>)</param>
+    /// <returns>IServiceCollection</returns>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+#pragma warning disable S1133 // Deprecated code should be removed
+    [Obsolete("Instead, use the built-in BrotliCompressor class (using AddBrotliCompressor) in the EasyCompressor package. (for projects with target-framework NETStandard2.1 or NETCore2.1 or above)")]
+#pragma warning restore S1133 // Deprecated code should be removed
+#endif
+    public static IServiceCollection AddBrotliNETCompressor(this IServiceCollection services, string name, uint quality = 5, uint window = 22)
+    {
+        services.TryAddSingleton<ICompressorProvider, DefaultCompressorProvider>();
+        services.AddSingleton<ICompressor, BrotliNETCompressor>(_ => new BrotliNETCompressor(name, quality, window));
+        return services;
     }
 }
