da83514e09c988fa2afd3062ac9fe75292e34da6
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-01-29 22:21:35 +0330

    LZ4BinaryCompressionMode added + implementation and performance improvement

---
+++ src/EasyCompressor.LZ4/LZ4BinaryCompressionMode.cs
@@ -0,0 +1,34 @@
+﻿// Ignore Spelling: LZ
+
+namespace EasyCompressor;
+
+/// <summary>
+/// LZ4 Binary Compression Mode
+/// </summary>
+public enum LZ4BinaryCompressionMode
+{
+    /// <summary>
+    /// Legacy compatibility with old/legacy versions. (NOT compatible with other modes neither StreamCompatible nor Fastest modes).
+    /// It's a bit faster than StreamCompatible but less efficient in memory allocation specially for large arrays
+    /// Note: It prepends 4 bytes to the beginning of the array to define the original array length.
+    /// </summary>
+    LegacyCompatible,
+
+    /// <summary>
+    /// Default compression mode which is compatible with Stream's output. (NOT compatible with other modes neither LegacyCompatible nor Fastest modes).
+    /// It's a bit slower than LegacyCompatible but more efficient in memory allocation specially for large arrays.
+    /// </summary>
+    /// <remarks>
+    /// More info: https://github.com/MiloszKrajewski/K4os.Compression.LZ4#other-stream-like-data-structures
+    /// </remarks>
+    StreamCompatible,
+
+    /// <summary>
+    /// The optimized mode. (NOT compatible with other modes neither LegacyCompatible nor StreamCompatible modes)
+    /// But it's the fastest and most efficient in memory allocation.
+    /// </summary>
+    /// <remarks>
+    /// More info: https://github.com/MiloszKrajewski/K4os.Compression.LZ4#pickler
+    /// </remarks>
+    Optimized
+}
--- src/EasyCompressor.LZ4/LZ4Compressor.cs
+++ src/EasyCompressor.LZ4/LZ4Compressor.cs
@@ -3,6 +3,8 @@
 using K4os.Compression.LZ4;
 using K4os.Compression.LZ4.Streams;
 using System;
+using System.Buffers;
+using System.ComponentModel;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,6 +17,14 @@ namespace EasyCompressor;
 public class LZ4Compressor : BaseCompressor
 {
     /// <summary>
+    /// Gets or sets the binary compression mode. (Defaults to <see cref="LZ4BinaryCompressionMode.StreamCompatible"/>)
+    /// </summary>
+    /// <value>
+    /// The binary compression mode.
+    /// </value>
+    public LZ4BinaryCompressionMode BinaryCompressionMode { get; set; }
+
+    /// <summary>
     /// LZ4Level
     /// </summary>
     protected readonly LZ4Level Level;
@@ -23,48 +33,93 @@ public class LZ4Compressor : BaseCompressor
     public override CompressionMethod Method => CompressionMethod.LZ4;
 
     /// <summary>
-    /// Initializes a new instance
+    /// Initializes a new instance of the <see cref="LZ4Compressor"/> class.
     /// </summary>
     /// <param name="name">Name</param>
-    /// <param name="level">LZ4Level</param>
-    public LZ4Compressor(string name = null, LZ4Level level = LZ4Level.L00_FAST)
+    /// <param name="level">The level.</param>
+    /// <param name="binaryCompressionMode">The binary compression mode. (Defaults to <see cref="LZ4BinaryCompressionMode.StreamCompatible"/>)</param>
+    public LZ4Compressor(string name = null, LZ4Level level = LZ4Level.L00_FAST, LZ4BinaryCompressionMode binaryCompressionMode = LZ4BinaryCompressionMode.StreamCompatible)
     {
         Name = name;
         Level = level;
+        BinaryCompressionMode = binaryCompressionMode;
     }
 
-#pragma warning disable IDE0057 // Use range operator (net462 and nestandard2.0 don't support System.Range)
     /// <inheritdoc/>
     protected override byte[] BaseCompress(byte[] bytes)
     {
-        var length = bytes.Length;
-        ReadOnlySpan<byte> source = bytes;
+        switch (BinaryCompressionMode)
+        {
+            case LZ4BinaryCompressionMode.LegacyCompatible:
+                {
+                    var length = bytes.Length;
+                    ReadOnlySpan<byte> source = bytes;
+                    var maxLength = LZ4Codec.MaximumOutputSize(length) + 4; // +4 bytes for specifying uncompressed length
 #if NET5_0_OR_GREATER
-        Span<byte> target = GC.AllocateUninitializedArray<byte>(LZ4Codec.MaximumOutputSize(length) + 4);
+                    Span<byte> target = GC.AllocateUninitializedArray<byte>(maxLength);
 #else
-        Span<byte> target = new byte[LZ4Codec.MaximumOutputSize(length) + 4];
+                    Span<byte> target = new byte[maxLength];
 #endif
-        Span<byte> size = BitConverter.GetBytes(length);
-        size.CopyTo(target);
-        int compressedLength = LZ4Codec.Encode(source, target.Slice(4), Level);
-        return target.Slice(0, compressedLength + 4).ToArray();
+                    Span<byte> size = BitConverter.GetBytes(length);
+                    size.CopyTo(target);
+#pragma warning disable IDE0057 // Use range operator (net462 and nestandard2.0 don't support System.Range)
+                    var compressedLength = LZ4Codec.Encode(source, target.Slice(4), Level);
+                    return target.Slice(0, compressedLength + 4).ToArray();
+#pragma warning restore IDE0057 // Use range operator (net462 and nestandard2.0 don't support System.Range)
+                }
+            case LZ4BinaryCompressionMode.StreamCompatible:
+                //https://github.com/MiloszKrajewski/K4os.Compression.LZ4#other-stream-like-data-structures
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+                var arrayBufferWriter = LZ4Frame.Encode(new Span<byte>(bytes), new ArrayBufferWriter<byte>(), Level);
+                return arrayBufferWriter.WrittenSpan.ToArray();
+#else
+                using (var outputStream = new MemoryStream())
+                {
+                    using (var arrayBufferWriter = LZ4Frame.Encode(outputStream, Level, leaveOpen: true))
+                        arrayBufferWriter.WriteManyBytes(bytes);
+                    return outputStream.GetTrimmedBuffer();
+                }
+#endif
+            case LZ4BinaryCompressionMode.Optimized:
+                //https://github.com/MiloszKrajewski/K4os.Compression.LZ4#pickler
+                return LZ4Pickler.Pickle((ReadOnlySpan<byte>)bytes, Level);
+            default:
+                throw new InvalidEnumArgumentException(nameof(BinaryCompressionMode), Convert.ToInt32(BinaryCompressionMode), typeof(LZ4BinaryCompressionMode));
+        }
     }
 
     /// <inheritdoc/>
     protected override byte[] BaseDecompress(byte[] compressedBytes)
     {
-        ReadOnlySpan<byte> source = compressedBytes;
-        var size = source.Slice(0, 4).ToArray();
-        var length = BitConverter.ToInt32(size, 0);
-#if NET5_0_OR_GREATER
-        Span<byte> target = GC.AllocateUninitializedArray<byte>(length);
+        switch (BinaryCompressionMode)
+        {
+            case LZ4BinaryCompressionMode.LegacyCompatible:
+                ReadOnlySpan<byte> source = compressedBytes;
+#pragma warning disable IDE0057 // Use range operator (net462 and nestandard2.0 don't support System.Range)
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+                var length = BitConverter.ToInt32(source.Slice(0, 4));
 #else
-        Span<byte> target = new byte[length];
+                var length = BitConverter.ToInt32(source.Slice(0, 4).ToArray(), 0);
 #endif
-        int decoded = LZ4Codec.Decode(source.Slice(4), target);
-        return target.Slice(0, decoded).ToArray();
-    }
+#if NET5_0_OR_GREATER
+                Span<byte> target = GC.AllocateUninitializedArray<byte>(length);
+#else
+                Span<byte> target = new byte[length];
+#endif
+                var decoded = LZ4Codec.Decode(source.Slice(4), target);
+                return target.Slice(0, decoded).ToArray();
 #pragma warning restore IDE0057 // Use range operator (net462 and nestandard2.0 don't support System.Range)
+            case LZ4BinaryCompressionMode.StreamCompatible:
+                //https://github.com/MiloszKrajewski/K4os.Compression.LZ4#other-stream-like-data-structures
+                using (var reader = LZ4Frame.Decode((ReadOnlyMemory<byte>)compressedBytes)) //new ReadOnlySequence<byte>(compressedBytes)
+                    return reader.AsStream().ReadAllBytes();
+            case LZ4BinaryCompressionMode.Optimized:
+                //https://github.com/MiloszKrajewski/K4os.Compression.LZ4#pickler
+                return LZ4Pickler.Unpickle((ReadOnlySpan<byte>)compressedBytes);
+            default:
+                throw new InvalidEnumArgumentException(nameof(BinaryCompressionMode), Convert.ToInt32(BinaryCompressionMode), typeof(LZ4BinaryCompressionMode));
+        }
+    }
 
     /// <inheritdoc/>
     protected override void BaseCompress(Stream inputStream, Stream outputStream)
@@ -118,5 +173,18 @@ public class LZ4Compressor : BaseCompressor
             //await lz4Stream.FlushAsync(cancellationToken).ConfigureAwait(false); //It's not necessary based on my experiments
         }
         await outputStream.FlushAsync(cancellationToken).ConfigureAwait(false); //It's needed because of FileStream internal buffering
+    }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        var mode = (BinaryCompressionMode) switch
+        {
+            LZ4BinaryCompressionMode.LegacyCompatible => "LegacyCompatible",
+            LZ4BinaryCompressionMode.StreamCompatible => "StreamCompatible",
+            LZ4BinaryCompressionMode.Optimized => "Optimized",
+            _ => throw new InvalidEnumArgumentException(nameof(BinaryCompressionMode), Convert.ToInt32(BinaryCompressionMode), typeof(LZ4BinaryCompressionMode)),
+        };
+        return $"{GetType().Name}-{mode}";
     }
 }
